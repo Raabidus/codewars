14839a11cf31ad5ad47e5a6c5c0900f64fe0610e
Raabidus <kralmartin93@gmail.com>
2024-05-06 15:26:58 +0200

    def no_space(x):     x_list = []     new_string = ""     x_list = x.rsplit(" ")     return new_string.join(x_list)

--- remove_spaces_from_string.py
+++ remove_spaces_from_string.py
@@ -1,1 +1,6 @@
 #  function that removes the spaces from the string, then return the resultant string
+def no_space(x):
+    x_list = []
+    new_string = ""
+    x_list = x.rsplit(" ")
+    return new_string.join(x_list)
